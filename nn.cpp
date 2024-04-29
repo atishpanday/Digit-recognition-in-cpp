def4d5c89747564f0f50d028330baca2f468ef4c
Atish <atishpanday1@gmail.com>
2024-04-29 16:02:38 -0700

    loss values not correct

--- nn.cpp
+++ nn.cpp
@@ -7,50 +7,34 @@
 #include <fstream>
 
 #define alpha 0.001
-#define epochs 100
-#define training_size 60000
-
-const int IMAGE_SIZE = 28 * 28; // Each image is 28x28 pixels
-const int LABEL_SIZE = 1;
+#define epochs 10
+#define training_size 10000
+#define image_size 28 * 28
 
 double sigmoid(double x) {
 	return (double) 1 / (1 + exp(-x));
 }
 
-double relu(double x) {
+/*double relu(double x) {
 	return x > 0 ? x : 0;
+}*/
+
+void read_mnist_train_image(std::ifstream& train_file, std::vector<double>& image, int i) {
+	train_file.seekg(4 * sizeof(int) + i * image_size, std::ios::beg);
+	std::vector<unsigned char> temp(image_size);
+	train_file.read(reinterpret_cast<char*>(temp.data()), image_size);
+	
+	for(int j = 0; j < image_size; j++) {
+		image[j] = static_cast<double>(temp[j]) / 255.0;
+	}
 }
 
-bool readMNISTFile(const std::string& filename, std::vector<double>& image, int& label) {
-    // Open the MNIST file in binary mode
-    std::ifstream file(filename, std::ios::binary);
-    if (!file) {
-        std::cerr << "Error opening file: " << filename << std::endl;
-        return false;
-    }
-
-    try {
-        // Read image data (pixels)
-        std::vector<unsigned char> image_bytes(IMAGE_SIZE);
-        file.read(reinterpret_cast<char*>(image_bytes.data()), IMAGE_SIZE);
-
-        // Convert image data to double values (normalize to range [0, 1])
-        image.resize(IMAGE_SIZE);
-        for (int i = 0; i < IMAGE_SIZE; ++i) {
-            image[i] = static_cast<double>(image_bytes[i]) / 255.0; // Normalize to [0, 1]
-        }
-
-        // Read label data
-        file.read(reinterpret_cast<int*>(&label), LABEL_SIZE);
-
-        // Close the file
-        file.close();
-    } catch (const std::exception& e) {
-        std::cerr << "Error reading file: " << filename << std::endl;
-        return false;
-    }
-
-    return true;
+unsigned char read_mnist_train_label(std::ifstream& label_file, int i) {
+	label_file.seekg(2 * sizeof(int) + i, std::ios::beg);
+	unsigned char label;
+	label_file.read(reinterpret_cast<char*>(&label), 1);
+	
+	return label;
 }
 
 class neuron {
@@ -65,8 +49,10 @@ class neuron {
 
     		// Create a uniform distribution between 0 and 1
     		std::uniform_real_distribution<double> dist(0.0, 1.0);
+    		
+    		weights.resize(K);
 		for(int i = 0; i < K; i++) {
-			weights.push_back(dist(gen));
+			weights[i] = dist(gen);
 			// bias.push_back(dist(gen));
 		}
 		value = 0;
@@ -74,7 +60,7 @@ class neuron {
 	
 	void calculate_value(std::vector<double>& x, int K) {
 		for(int i = 0; i < K; i++) {
-			value += weight[i] * x[i] + bias[i];
+			value += weights[i] * x[i]; // + bian[i];
 		}
 		
 		value = sigmoid(value);
@@ -98,7 +84,7 @@ class neuron {
 		return value;
 	}
 	
-	~neuron();
+	// ~neuron();
 };
 
 class layer {
@@ -114,9 +100,12 @@ class layer {
 	}
 	
 	void calculate_values(std::vector<double>& x, int K) {
+		values.resize(K);
+		int i = 0;
 		for(neuron n:neurons) {
-			n.calculate_value(x, K)
-			values.append(n.get_value());
+			n.calculate_value(x, K);
+			values[i] = n.get_value();
+			i++;
 		}
 	}
 	
@@ -130,58 +119,55 @@ class layer {
 	
 	void calculate_loss(std::vector<double>& t) {
 		double y;
+		int i = 0;
 		for(neuron n:neurons) {
 			y = n.get_value();
-			loss += (t - y) * (t - y);
+			loss += (t[i] - y) * (t[i] - y);
+			i++;
 		}
 		loss /= 2;
 	}
 	
 	void update_weights(std::vector<double>& x, std::vector<double>& t, int K) {
+		calculate_loss(t);
+		int i = 0;
 		for(neuron n:neurons) {
-			n.update_weights(x, t, K);
+			n.update_weights(x, t[i], K);
+			i++;
 		}
 	}
 	
-	~layer();
+	// ~layer();
 };
 
-/*class sequential_nn {
-	std::vector<layer> layers;
-	
-	public:
-	sequential_nn(int input, int n1, int n2, int n3) {
-		layers.append(layer(input, n1));
-		layers.append(layer(n1, n2));
-		layers.append(layer(n2, n3));
-	}
-};*/
-	
-
 int main() {
-	std::vector<double> image;
+	std::vector<double> image(image_size);
+	double label;	
+	std::string train_image_file = "train-images-idx3-ubyte";
+	std::string train_label_file = "train-labels-idx1-ubyte";
 	
-	std::vector<double> labels;
-	labels.resize(training_size);
-	
-	// std::vector<double> predictions;
-	// predictions.resize(training_size);
+	std::ifstream tif(train_image_file, std::ios::binary);
+	std::ifstream tlf(train_label_file, std::ios::binary);
 	
 	// Initializing the layers
-	layer layer1 = layer(image.size(), 128);
+	layer layer1 = layer(image_size, 128);
 	layer layer2 = layer(128, 64);
 	layer layer3 = layer(64, 10);
 	layer output_layer = layer(10, 1);
 	
-	std::ofstream loss("./loss.txt");
+	double avg_loss;
+	
+	std::ofstream loss("loss.txt");
 	
 	int ep = 0;
 	
 	while(ep < epochs) {
 		// Assuming image has stored 1 image data from the MNIST dataset
+		avg_loss = 0;
 		int i = 0;
 		while(i < training_size) {
-			readMNISTFile("", image, labels[i]);
+			read_mnist_train_image(tif, image, i);
+			label = static_cast<double>(read_mnist_train_label(tlf, i));
 			
 			layer1.calculate_values(image, image.size());
 			layer2.calculate_values(layer1.get_values(), layer1.get_values().size());
@@ -189,21 +175,25 @@ int main() {
 			output_layer.calculate_values(layer3.get_values(), layer3.get_values().size());
 			
 			// predictions[i] = output_layer.get_values()[0];
-			std::vector<double> temp_label = {labels[i]};
+			std::vector<double> temp_label = {label};
 			output_layer.update_weights(layer3.get_values(), temp_label, layer3.get_values().size());
 			layer3.update_weights(layer2.get_values(), output_layer.get_values(), layer2.get_values().size());
 			layer2.update_weights(layer1.get_values(), layer3.get_values(), layer1.get_values().size());
 			layer1.update_weights(image, layer2.get_values(), image.size());
 			
+			avg_loss += output_layer.get_loss();
+			
 			i++;
 		}
 		
-		loss << output_layer.get_values()[0] << std::endl;
+		avg_loss /= training_size;
 		
+		loss << avg_loss << std::endl;
+		std::cout << "\nEpoch: " << ep;
 		ep++;
 	}
 	
-	file.close();
+	loss.close();
 		
 	
 	return 0;
