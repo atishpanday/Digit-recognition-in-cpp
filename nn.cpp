291e90e48e19f58b0f2d592baa1d55782b19b1a7
Atish <atishpanday1@gmail.com>
2024-04-30 01:19:06 -0700

    loss not low enough, predictions are bad

--- nn.cpp
+++ nn.cpp
@@ -7,12 +7,12 @@
 #include <fstream>
 
 #define alpha 0.001
-#define epochs 10
-#define training_size 10000
-#define image_size 28 * 28
+const int epochs = 10;
+const int training_size = 40000;
+const int image_size = 28 * 28;
 
 double sigmoid(double x) {
-	return (double) 1 / (1 + exp(-x));
+	return 1 / (1 + exp(-x));
 }
 
 /*double relu(double x) {
@@ -38,6 +38,7 @@ unsigned char read_mnist_train_label(std::ifstream& label_file, int i) {
 }
 
 class neuron {
+	int num_weights;
 	double value;
 	std::vector<double> weights;
 	// std::vector<double> bias;
@@ -50,35 +51,27 @@ class neuron {
     		// Create a uniform distribution between 0 and 1
     		std::uniform_real_distribution<double> dist(0.0, 1.0);
     		
-    		weights.resize(K);
+    		num_weights = K;
+    		weights.reserve(K);
 		for(int i = 0; i < K; i++) {
 			weights[i] = dist(gen);
 			// bias.push_back(dist(gen));
 		}
-		value = 0;
 	}
 	
-	void calculate_value(std::vector<double>& x, int K) {
-		for(int i = 0; i < K; i++) {
-			value += weights[i] * x[i]; // + bian[i];
+	void calculate_value(std::vector<double>& x) {
+		for(int i = 0; i < num_weights; i++) {
+			value += weights[i] * x[i]; // + bias[i];
 		}
 		
 		value = sigmoid(value);
 	}
 	
-	void update_weights(std::vector<double>& x, double t, int K) {
-		for(int i = 0; i < K; i++) {
+	void update_weights(std::vector<double>& x, double t) {
+		for(int i = 0; i < num_weights; i++) {
 			weights[i] -= alpha * (value - t) * value * (1 - value) * x[i];
 		}
 	}
-	
-	double get_weight(int i) {
-		return weights[i];
-	}
-	
-	//double get_bias(int i) {
-	//	return bias[i];
-	//}
 	
 	double get_value() {
 		return value;
@@ -88,57 +81,66 @@ class neuron {
 };
 
 class layer {
+	int num_neurons;
 	std::vector<neuron> neurons;
 	std::vector<double> values;
 	double loss;
 	
 	public:
 	layer(int K, int N) {
+		num_neurons = N;
+		neurons.reserve(N);
+		values.reserve(N);
 		for(int i = 0; i < N; i++) {
-			neurons.push_back(neuron(K));
+			neurons.emplace_back(K);
 		}
 	}
 	
-	void calculate_values(std::vector<double>& x, int K) {
-		values.resize(K);
-		int i = 0;
-		for(neuron n:neurons) {
-			n.calculate_value(x, K);
-			values[i] = n.get_value();
-			i++;
+	void calculate_values(std::vector<double>& x) {
+		for(int i = 0; i < num_neurons; i++) {
+			neurons[i].calculate_value(x);
+			values[i] = neurons[i].get_value();
 		}
+	}
+	
+	void update_weights(std::vector<double>& x, std::vector<double>& t) {
+		for(int i = 0; i < num_neurons; i++) {
+			neurons[i].update_weights(x, t[i]);
+		}
+		calculate_loss(t);
+	}
+
+	void calculate_loss(std::vector<double>& t) {
+		double y;
+		for(int i = 0; i < num_neurons; i++) {
+			y = neurons[i].get_value();
+			loss += (t[i] - y) * (t[i] - y);
+		}
+		loss /= 2 * num_neurons;
+	}
+
+	int get_num_neurons() {
+		return num_neurons;
+	}
+
+	std::vector<double>& get_values() {
+		return values;
 	}
 	
 	double get_loss() {
 		return loss;
 	}
-	
-	std::vector<double>& get_values() {
-		return values;
-	}
-	
-	void calculate_loss(std::vector<double>& t) {
-		double y;
-		int i = 0;
-		for(neuron n:neurons) {
-			y = n.get_value();
-			loss += (t[i] - y) * (t[i] - y);
-			i++;
-		}
-		loss /= 2;
-	}
-	
-	void update_weights(std::vector<double>& x, std::vector<double>& t, int K) {
-		calculate_loss(t);
-		int i = 0;
-		for(neuron n:neurons) {
-			n.update_weights(x, t[i], K);
-			i++;
-		}
-	}
-	
 	// ~layer();
 };
+
+double predict(std::vector<double>& image, layer& layer1, layer& layer2, layer& layer3, layer& output_layer) {
+	layer1.calculate_values(image);
+	layer2.calculate_values(layer1.get_values());
+	layer3.calculate_values(layer2.get_values());
+	output_layer.calculate_values(layer3.get_values());
+	
+	return output_layer.get_values()[0] * 10;
+}
 
 int main() {
 	std::vector<double> image(image_size);
@@ -169,17 +171,17 @@ int main() {
 			read_mnist_train_image(tif, image, i);
 			label = static_cast<double>(read_mnist_train_label(tlf, i));
 			
-			layer1.calculate_values(image, image.size());
-			layer2.calculate_values(layer1.get_values(), layer1.get_values().size());
-			layer3.calculate_values(layer2.get_values(), layer2.get_values().size());
-			output_layer.calculate_values(layer3.get_values(), layer3.get_values().size());
+			layer1.calculate_values(image);
+			layer2.calculate_values(layer1.get_values());
+			layer3.calculate_values(layer2.get_values());
+			output_layer.calculate_values(layer3.get_values());
 			
 			// predictions[i] = output_layer.get_values()[0];
-			std::vector<double> temp_label = {label};
-			output_layer.update_weights(layer3.get_values(), temp_label, layer3.get_values().size());
-			layer3.update_weights(layer2.get_values(), output_layer.get_values(), layer2.get_values().size());
-			layer2.update_weights(layer1.get_values(), layer3.get_values(), layer1.get_values().size());
-			layer1.update_weights(image, layer2.get_values(), image.size());
+			std::vector<double> temp_label = {label / 10};
+			output_layer.update_weights(layer3.get_values(), temp_label);
+			layer3.update_weights(layer2.get_values(), output_layer.get_values());
+			layer2.update_weights(layer1.get_values(), layer3.get_values());
+			layer1.update_weights(image, layer2.get_values());
 			
 			avg_loss += output_layer.get_loss();
 			
@@ -189,11 +191,21 @@ int main() {
 		avg_loss /= training_size;
 		
 		loss << avg_loss << std::endl;
-		std::cout << "\nEpoch: " << ep;
+		std::cout << "Epoch: " << ep << "\n";
 		ep++;
 	}
 	
 	loss.close();
+	
+	read_mnist_train_image(tif, image, training_size + 10);
+	label = static_cast<double>(read_mnist_train_label(tlf, training_size + 10));
+	
+	double prediction = std::round(predict(image, layer1, layer2, layer3, output_layer));
+	
+	std::cout << "\nPredicted value: " << prediction << "\nTrue value: " << label << "\n";
+	
+	tif.close();
+	tlf.close();
 		
 	
 	return 0;
